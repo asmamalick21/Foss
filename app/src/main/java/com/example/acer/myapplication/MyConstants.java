11284617cb7742ea1ba39a2d12838d28780adbf7
asmamalick21 <asmamalick21@gmail.com>
2016-05-13 10:13:09 +0530

    Update MyConstants.java
    to keep the constant names precise

--- app/src/main/java/com/example/acer/myapplication/MyConstants.java
+++ app/src/main/java/com/example/acer/myapplication/MyConstants.java
@@ -2,9 +2,11 @@ package com.example.user.alarmclock;
 
 public abstract class MyConstants {
 
-    public static final String HOUR = "time_hour";
+    public static final String HOUR = "hour";
 
-    public static final String MINUTE = "time_minute";
+    public static final String MINUTE = "minute";
+    
+    public static final String SECONDS = "second";
 
     public static final String TIME_PICKER = "time_picker";
 
